b122c154ccf2a5d0bc223f124c441290245cad3e
guintoki <guilherme-vilas-boas@hotmail.com>
2020-03-03 10:59:30 -0300

    init notifications

--- src/database/migrations/20200302142100-create-appointments.js
+++ src/database/migrations/20200302142100-create-appointments.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('files', {
+    return queryInterface.createTable('appointments', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
