c7456a2487c958767b48519b876dfe351511b7a8
guintoki <guilherme-vilas-boas@hotmail.com>
2020-03-18 15:12:31 -0300

    notifications

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fa542ec4dc346d',
+    pass: 'e654e7a6057f3f',
+  },
+  default: {
+    from: 'Guilherme GoBarber <noreply@gobarber.com>',
+  },
+};
